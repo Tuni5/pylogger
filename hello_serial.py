dcee436ca2064185963f8dc5f64e059595047b2c
Tuni5 <antony.pfoertzsch@gmail.com>
2026-08-09 22:00:07 +0200

    First bytes through loopback since 2021

---
+++ hello_serial.py
@@ -0,0 +1,11 @@
+import serial       
+
+port = serial.serial_for_url("loop://", timeout=1)
+
+
+
+frame = bytes([0xAA, 0x55, 0x04, 0x2A, 0x00, 0x0F, 0xFF])
+port.write(frame)
+
+received = port.read(len(frame))
+print(" ".join(f"[{b:02X}]" for b in received))
